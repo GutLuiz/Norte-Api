9042a9150433068f3e6956c36ed2a481e77c3e88
GutLuiz <gutfoda@gmail.com>
2026-09-11 17:49:37 -0300

    resumo de dados da revisão

---
+++ UepaMed.Application/Dtos/Revisoes/ResumoDadosRevisaoDto.cs
@@ -0,0 +1,17 @@
+﻿namespace UepaMed.Application.Dtos.Revisoes
+{
+    public class ResumoDadosRevisaoDto
+    {
+        public int ArquivosImportados { get; set; }
+
+        public int ArtigosDuplicados { get; set; }
+
+        public int ArtigosPendentes { get; set; }
+
+        public int ArtigosIncluidos { get; set; }
+
+        public int ArtigosExcluidos { get; set; }
+
+        public int ConflitosIdentificados { get; set; }
+    }
+}
