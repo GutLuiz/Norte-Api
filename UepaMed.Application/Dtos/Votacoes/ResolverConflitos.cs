376fb9b8408f6cbdfdac5af41c71ae335986b02d
GutLuiz <gutfoda@gmail.com>
2026-09-11 17:48:59 -0300

    DTOS para o corpo de artigos em conflito e ações com eles

---
+++ UepaMed.Application/Dtos/Votacoes/ResolverConflitos.cs
@@ -0,0 +1,9 @@
+﻿using UepaMed.Domain.Enums.Votacao;
+
+namespace UepaMed.Application.Dtos.Votacoes
+{
+    public class ResolverConflitoDto
+    {
+        public OpcaoVoto DecisaoFinal { get; set; }
+    }
+}
