4e21734317a96de1ec785bb837af5bd23910d226
GutLuiz <gutfoda@gmail.com>
2026-09-11 17:50:46 -0300

    artigos em conflito

---
+++ UepaMed.Application/Dtos/Artigos/ArtigoConflitoRespostaDto.cs
@@ -0,0 +1,20 @@
+﻿using UepaMed.Application.Dtos.Artigos;
+using UepaMed.Domain.Enums.Votacao;
+
+namespace UepaMed.Application.Dtos.Votacoes
+{
+    public class ArtigoConflitoRespostaDto
+    {
+        public int ConflitoId { get; set; }
+
+        public int VotacaoId { get; set; }
+
+        public int ArtigoId { get; set; }
+
+        public MotivoConflito Motivo { get; set; }
+
+        public bool Resolvido { get; set; }
+
+        public ArtigoComparacaoDto Artigo { get; set; } = null!;
+    }
+}
